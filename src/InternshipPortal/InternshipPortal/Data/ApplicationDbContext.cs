31f0f60a078e3773d6c45001513505b24a0b7a48
Abdullah Cemal YILDIRIM <yldrmmabdullah@gmail.com>
2019-05-08 23:01:11 +0300

    Resume

--- src/InternshipPortal/InternshipPortal/Data/ApplicationDbContext.cs
+++ src/InternshipPortal/InternshipPortal/Data/ApplicationDbContext.cs
@@ -14,5 +14,6 @@ namespace InternshipPortal.Data
         {
         }
         public DbSet<InternshipPortal.Models.Job> Job { get; set; }
+        public DbSet<InternshipPortal.Models.Resumes> Resumes { get; set; }
     }
 }
